607b9a79c6b54cf51b9ef056f1e6708f0c81008a
Jim Hall <jim.hall.dev@gmail.com>
2022-02-04 11:11:21 -0500

    configure api for local testing

--- 3-full-stack/front-end/src/modules/api.js
+++ 3-full-stack/front-end/src/modules/api.js
@@ -1,4 +1,5 @@
-const basePath = '/api';
+const basePath = 'http://localhost:5000/api'; // use localhost when local
+// const basePath = '/api'; // switch to /api before deploying to ec2
 
 export const API = async(method, path, payload) => {
   const url = `${basePath}${path}`;
