71eb630611ed364c7392cbc59376168887bb2f79
Jim Hall <jim.hall.dev@gmail.com>
2022-02-11 09:35:52 -0500

    return to front-end and backend folders

--- 4-full-stack-lambdas/front-end/amplify/backend/function/blogapi/src/index.js
+++ 4-full-stack-lambdas/front-end/amplify/backend/function/blogapi/src/index.js
@@ -1,17 +1,3 @@
-/*
-Use the following code to retrieve configured secrets from SSM:
-
-const aws = require('aws-sdk');
-
-const { Parameters } = await (new aws.SSM())
-  .getParameters({
-    Names: ["dbuser","dbpass","allowed_client"].map(secretName => process.env[secretName]),
-    WithDecryption: true,
-  })
-  .promise();
-
-Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
-*/
 require("core-js/stable");
 require("regenerator-runtime/runtime");
 const awsServerlessExpress = require('aws-serverless-express');
